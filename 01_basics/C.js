f21c18a6134ae06214503d8c2d411156219ae3c6
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-02-20 11:12:48 +0000

    datatype

---
+++ 01_basics/C.js
@@ -0,0 +1,18 @@
+let name = "hitesh"
+let age = 18
+let isloggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigInt
+//string => true/false
+//null =stand alone value
+//undefined => 
+    //symbol = unique
+
+//object
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof isloggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
